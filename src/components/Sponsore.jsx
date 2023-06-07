3d3360ce97fd3bab90dfd26686cf1264d4e4ed50
yayahc <yayahc@localhost.localdomain>
2023-06-07 23:58:49 +0000

    react-player added#2

--- src/components/Sponsore.jsx
+++ src/components/Sponsore.jsx
@@ -15,19 +15,19 @@ const Sponsore = () => {
                 <div className='sponsors_description'>
                     <p>We are incredibly grateful for our sponsors, who make our work possible. For Now, Grassroot Academy  and The PBS learning foundation are the only support systems we have  without which our work will not be possible </p>
                     <div className='sponsors-img'>
-                    <img src={logo1} className="img-custom-border" alt="" />
-                    <img src={logo2} className="img-custom-border" alt="" />
-                    <img src={spon} className="img-custom-border" alt="" />
+                        <img src={logo1} className="img-custom-border" alt="" />
+                        <img src={logo2} className="img-custom-border" alt="" />
+                        <img src={spon} className="img-custom-border" alt="" />
                     </div>
 
                     <div className='react-player'>
-      <ReactPlayer
-        url="https://vimeo.com/728147749"
-        controls="true"
-        width='100%'
-        height='100%'
-      />
-    </div>
+                        <ReactPlayer
+                            url="https://vimeo.com/728147749"
+                            controls="true"
+                            width='100%'
+                            height='100%'
+                        />
+                    </div>
 
                     <br />
                     <p>We  solicit support from agencies, companies and individuals.</p>
