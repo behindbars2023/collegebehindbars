c91dd97f69c56625fc4ec9cd24c0ca97abd2e418
yayahc <hamedcuenca5@gmail.com>
2023-06-11 18:21:56 +0000

    link added

--- src/components/Sponsore.jsx
+++ src/components/Sponsore.jsx
@@ -14,10 +14,15 @@ const Sponsore = () => {
                 </div>
                 <div className='sponsors_description'>
                     <p>We are incredibly grateful for our sponsors, who make our work possible. For Now, Grassroot Academy  and The PBS learning foundation are the only support systems we have  without which our work will not be possible </p>
-                    <div className='sponsors-img'>
+                    <div className='sponsors-img'>        
                         <img src={logo1} className="img-custom-border" alt="" />
+                        <a target="_blank" className='v-bpi' href="https://grassroot.academy/">visit Grassroot</a>
+
                         <img src={logo2} className="img-custom-border" alt="" />
+                        <a target="_blank"  className='v-bpi' href="https://www.pbs.org/show/college-behind-bars/">visit PBS</a>
+
                         <img src={spon} className="img-custom-border" alt="" />
+                        <a target="_blank"  className='v-bpi' href="https://bpi.bard.edu/">visit BPI</a>
                     </div>
 
                     <div className='react-player'>
