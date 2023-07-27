18f057fcae73b5095f595bbc960cfc06bb8486b9
behindbars2023 <135469910+behindbars2023@users.noreply.github.com>
2023-07-27 10:32:45 +0000

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Navbar from "./Navbar";
 import MainHeader from "./MainHeader";
-import About from "./About"
+/*import About from "./About"*/
 import HowItWorks from "./HowItWorks"
 import Challenges from "./Challenges"
 import FormCustom from "./FormCustom"
 import Testimonials from "./Testimonials";
 import Sample from "./Sample";
-import Partners from "./Partners";
-import Sponsore from "./Sponsore";
+/*import Partners from "./Partners"*/
+/* import Sponsore from "./Sponsore";*/
 import Programs from "./Programs"
-import Donate from "./Donate"
+/*import Donate from "./Donate"*/
 import Footer from "./Footer"
 import Missions from "./Missions";
 
